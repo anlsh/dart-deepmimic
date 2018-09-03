8391ef2632e24415d6b89c07247edc209c2e9f45
anish <anish.moorthy@gmail.com>
2018-09-03 12:55:06 -0400

    Modularize some stuff

--- dartdeepmimic.py
+++ dartdeepmimic.py
@@ -607,88 +607,112 @@ class DartDeepMimicEnv(dart_env.DartEnv):
         elif mode == 'human':
             self._get_viewer().runSingleStep()
 
+class DartDeepMimicArgParse(argparse.ArgumentParser):
+
+    def __init__(self):
+        super().__init__()
+        self.add_argument('--control-skel-path', required=True,
+                          help='Path to the control skeleton')
+        self.add_argument('--asf-path', required=True,
+                          help='Path to asf which the skeleton was parsed from')
+        self.add_argument('--ref-motion-path', required=True,
+                          help='Path to the reference motion AMC')
+        self.add_argument('--state-mode', default=0, type=int,
+                          help="Code for the state representation")
+        self.add_argument('--action-mode', default=0, type=int,
+                          help="Code for the action representation")
+        self.add_argument('--visualize', default=False,
+                          help="DOESN'T DO ANYTHING RIGHT NOW: True if you want a window to render to")
+        self.add_argument('--max-torque', type=float, default=90,
+                          help="Maximum torque")
+        self.add_argument('--default-damping', type=float, default=80,
+                          help="Default damping coefficient for joints")
+        self.add_argument('--default-spring', type=float, default=0,
+                          help="Default spring stiffness for joints")
+        self.add_argument('--simsteps-per-dataframe', type=int, default=10,
+                          help="Number of simulation steps per frame of mocap" +
+                          " data")
+        self.add_argument('--reward-cutoff', type=float, default=0.1,
+                          help="Terminate the episode when rewards below this " +
+                          "threshold are calculated. Should be in range (0, 1)")
+        self.add_argument('--window-width', type=int, default=80,
+                          help="Window width")
+        self.add_argument('--window-height', type=int, default=45,
+                          help="Window height")
+
+        self.add_argument('--pos-init-noise', type=float, default=.05,
+                          help="Standard deviation of the position init noise")
+        self.add_argument('--vel-init-noise', type=float, default=.05,
+                          help="Standart deviation of the velocity init noise")
+
+        self.add_argument('--pos-weight', type=float, default=.65,
+                          help="Weighting for the pos difference in the reward")
+        self.add_argument('--pos-inner-weight', type=float, default=-2,
+                          help="Coefficient for pos difference exponentiation in reward")
+
+        self.add_argument('--vel-weight', type=float, default=.1,
+                          help="Weighting for the pos difference in the reward")
+        self.add_argument('--vel-inner-weight', type=float, default=-.1,
+                          help="Coefficient for vel difference exponentiation in reward")
+
+        self.add_argument('--ee-weight', type=float, default=.15,
+                          help="Weighting for the pos difference in the reward")
+        self.add_argument('--ee-inner-weight', type=float, default=-40,
+                          help="Coefficient for pos difference exponentiation in reward")
+
+        self.add_argument('--com-weight', type=float, default=.1,
+                          help="Weighting for the com difference in the reward")
+        self.add_argument('--com-inner-weight', type=float, default=-10,
+                          help="Coefficient for com difference exponentiation in reward")
+
+        self.add_argument('--p-gain', type=float, default=300,
+                            help="P for the PD controller")
+        self.add_argument('--d-gain', type=float, default=50,
+                          help="D for the PD controller")
+
+        self.args = None
+
+    def parse_args(self):
+        self.args = super().parse_args()
+        return self.args
+
+    def get_env(self):
+
+        return DartDeepMimicEnv(self.args.control_skel_path,
+                                self.args.asf_path,
+                                self.args.ref_motion_path,
+                                self.args.state_mode,
+                                self.args.action_mode,
+                                self.args.p_gain, self.args.d_gain,
+                                self.args.pos_init_noise,
+                                self.args.vel_init_noise,
+                                self.args.reward_cutoff,
+                                self.args.pos_weight,
+                                self.args.pos_inner_weight,
+                                self.args.vel_weight,
+                                self.args.vel_inner_weight,
+                                self.args.ee_weight,
+                                self.args.ee_inner_weight,
+                                self.args.com_weight,
+                                self.args.com_inner_weight,
+                                self.args.max_torque,
+                                self.args.default_damping,
+                                self.args.default_spring,
+                                self.args.visualize,
+                                self.args.simsteps_per_dataframe,
+                                self.args.window_width,
+                                self.args.window_height)
+
 
 if __name__ == "__main__":
 
     # Don't run this as main, there's really not too much point
 
-    parser = argparse.ArgumentParser(description='Make a DartDeepMimic Environ')
-    parser.add_argument('--control-skel-path', required=True,
-                        help='Path to the control skeleton')
-    parser.add_argument('--asf-path', required=True,
-                        help='Path to asf which the skeleton was parsed from')
-    parser.add_argument('--ref-motion-path', required=True,
-                        help='Path to the reference motion AMC')
-    parser.add_argument('--state-mode', default=0, type=int,
-                        help="Code for the state representation")
-    parser.add_argument('--action-mode', default=0, type=int,
-                        help="Code for the action representation")
-    parser.add_argument('--visualize', default=False,
-                        help="DOESN'T DO ANYTHING RIGHT NOW: True if you want a window to render to")
-    parser.add_argument('--max-torque', type=float, default=90,
-                        help="Maximum torque")
-    parser.add_argument('--default-damping', type=float, default=80,
-                        help="Default damping coefficient for joints")
-    parser.add_argument('--default-spring', type=float, default=0,
-                        help="Default spring stiffness for joints")
-    parser.add_argument('--simsteps-per-dataframe', type=int, default=10,
-                        help="Number of simulation steps per frame of mocap" +
-                        " data")
-    parser.add_argument('--reward-cutoff', type=float, default=0.1,
-                        help="Terminate the episode when rewards below this " +
-                             "threshold are calculated. Should be in range (0, 1)")
-    parser.add_argument('--window-width', type=int, default=80,
-                        help="Window width")
-    parser.add_argument('--window-height', type=int, default=45,
-                        help="Window height")
-
-    parser.add_argument('--pos-init-noise', type=float, default=.05,
-                        help="Standard deviation of the position init noise")
-    parser.add_argument('--vel-init-noise', type=float, default=.05,
-                        help="Standart deviation of the velocity init noise")
-
-    parser.add_argument('--pos-weight', type=float, default=.65,
-                        help="Weighting for the pos difference in the reward")
-    parser.add_argument('--pos-inner-weight', type=float, default=-2,
-                        help="Coefficient for pos difference exponentiation in reward")
-
-    parser.add_argument('--vel-weight', type=float, default=.1,
-                        help="Weighting for the pos difference in the reward")
-    parser.add_argument('--vel-inner-weight', type=float, default=-.1,
-                        help="Coefficient for vel difference exponentiation in reward")
-
-    parser.add_argument('--ee-weight', type=float, default=.15,
-                        help="Weighting for the pos difference in the reward")
-    parser.add_argument('--ee-inner-weight', type=float, default=-40,
-                        help="Coefficient for pos difference exponentiation in reward")
-
-    parser.add_argument('--com-weight', type=float, default=.1,
-                        help="Weighting for the com difference in the reward")
-    parser.add_argument('--com-inner-weight', type=float, default=-10,
-                        help="Coefficient for com difference exponentiation in reward")
-
-    parser.add_argument('--p-gain', type=float, default=300,
-                        help="P for the PD controller")
-    parser.add_argument('--d-gain', type=float, default=50,
-                        help="D for the PD controller")
+    parser = DartDeepMimicArgParse()
 
     args = parser.parse_args()
 
-    env = DartDeepMimicEnv(args.control_skel_path, args.asf_path,
-                           args.ref_motion_path,
-                           args.state_mode, args.action_mode,
-                           args.p_gain, args.d_gain,
-                           args.pos_init_noise, args.vel_init_noise,
-                           args.reward_cutoff,
-                           args.pos_weight, args.pos_inner_weight,
-                           args.vel_weight, args.vel_inner_weight,
-                           args.ee_weight, args.ee_inner_weight,
-                           args.com_weight, args.com_inner_weight,
-                           args.max_torque,
-                           args.default_damping, args.default_spring,
-                           args.visualize,
-                           args.simsteps_per_dataframe,
-                           args.window_width, args.window_height)
+    env = parser.get_env()
 
     # obs = env.reset(0, False)
     # done = False
--- train_dartdeepmimic.py
+++ train_dartdeepmimic.py
@@ -7,16 +7,43 @@ from baselines.bench import Monitor
 from baselines import logger
 import argparse
 
-from dartdeepmimic import DartDeepMimicEnv
+from dartdeepmimic import DartDeepMimicEnv, DartDeepMimicArgParse
 
-def train(env, save_interval, file_prefix, num_timesteps):
+
+# class PolicyLoaderAgent(object):
+#     """The world's simplest agent!"""
+#     def __init__(self, param_path, obs_space, action_space):
+#         self.action_space = action_space
+
+#         # TODO These parameters are COUPLED: should match up with those in
+#         # run_dart.py which is over in baselines
+#         self.actor = mlp_policy.MlpPolicy("pi", obs_space, action_space,
+#                                           hid_size = 64, num_hid_layers=2)
+#         U.initialize()
+#         U.load_state(param_path)
+
+#     def act(self, observation, reward, done):
+#         # action1 = self.action_space.sample()
+#         action2, unknown = self.actor.act(False, observation)
+#         return action2
+
+def train(env, initial_params_path,
+          save_interval, out_prefix, num_timesteps, num_cpus):
     from baselines.ppo1 import mlp_policy, pposgd_simple
     #with tf.variable_scope('inclined'):
-    U.make_session(num_cpu=1).__enter__()
+    U.make_session(num_cpu=num_cpus).__enter__()
+
+    U.initialize()
+
     #set_global_seeds(seed)
     def policy_fn(name, ob_space, ac_space):
-        return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+        print("Policy with name: ", name)
+        policy = mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
             hid_size=64, num_hid_layers=2)
+        if initial_params_path is not None:
+            U.load_state(initial_params_path)
+        return policy
+
     #env = bench.Monitor(env, "results.json")
     #env.seed(seed)
     gym.logger.setLevel(logging.WARN)
@@ -26,7 +53,7 @@ def train(env, save_interval, file_prefix, num_timesteps):
         # TODO also probably dont save on every single iteration...
         iters = local_vars["iters_so_far"]
         if iters % save_interval == 0:
-            U.save_state(file_prefix + str(iters))
+            U.save_state(out_prefix + str(iters))
 
     pposgd_simple.learn(env, policy_fn,
             max_timesteps=num_timesteps,
@@ -40,99 +67,27 @@ def train(env, save_interval, file_prefix, num_timesteps):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Make a DartDeepMimic Environ')
-    parser.add_argument('--train-save-interval', type=int, default=25,
+    parser = DartDeepMimicArgParse()
+    parser.add_argument('--train-save-interval', type=int, default=100,
                         help="Interval between saves and stuff")
-    parser.add_argument('--train-params-prefix', required=True,
+    parser.add_argument('--initial-params-path', type=str, default=None,
+                        help="Path to the prefix of files you want to load from")
+    parser.add_argument('--output-params-prefix', required=True,
                         help="Fire prefix of parameter saves")
+    parser.add_argument('--num-cpus', type=int, default=1,
+                        help="Number of CPU cores to use? Idk...")
     parser.add_argument('--train-num-timesteps', type=int, default=7e8,
                         help="No idea what this does")
 
-
-    #######################################
-    # BEGIN COPY-PASTE FROM DARTDEEPMIMIC #
-    #######################################
-
-    parser.add_argument('--control-skel-path', required=True,
-                        help='Path to the control skeleton')
-    parser.add_argument('--asf-path', required=True,
-                        help='Path to asf which the skeleton was parsed from')
-    parser.add_argument('--ref-motion-path', required=True,
-                        help='Path to the reference motion AMC')
-    parser.add_argument('--state-mode', default=0, type=int,
-                        help="Code for the state representation")
-    parser.add_argument('--action-mode', default=0, type=int,
-                        help="Code for the action representation")
-    parser.add_argument('--visualize', default=False,
-                        help="DOESN'T DO ANYTHING RIGHT NOW: True if you want a window to render to")
-    parser.add_argument('--max-torque', type=float, default=90,
-                        help="Maximum torque")
-    parser.add_argument('--default-damping', type=float, default=80,
-                        help="Default damping coefficient for joints")
-    parser.add_argument('--default-spring', type=float, default=0,
-                        help="Default spring stiffness for joints")
-    parser.add_argument('--simsteps-per-dataframe', type=int, default=10,
-                        help="Number of simulation steps per frame of mocap" +
-                        " data")
-    parser.add_argument('--reward-cutoff', type=float, default=0.1,
-                        help="Terminate the episode when rewards below this " +
-                             "threshold are calculated. Should be in range (0, 1)")
-    parser.add_argument('--window-width', type=int, default=80,
-                        help="Window width")
-    parser.add_argument('--window-height', type=int, default=45,
-                        help="Window height")
-
-    parser.add_argument('--pos-init-noise', type=float, default=.05,
-                        help="Standard deviation of the position init noise")
-    parser.add_argument('--vel-init-noise', type=float, default=.05,
-                        help="Standart deviation of the velocity init noise")
-
-    parser.add_argument('--pos-weight', type=float, default=.65,
-                        help="Weighting for the pos difference in the reward")
-    parser.add_argument('--pos-inner-weight', type=float, default=-2,
-                        help="Coefficient for pos difference exponentiation in reward")
-
-    parser.add_argument('--vel-weight', type=float, default=.1,
-                        help="Weighting for the pos difference in the reward")
-    parser.add_argument('--vel-inner-weight', type=float, default=-.1,
-                        help="Coefficient for vel difference exponentiation in reward")
-
-    parser.add_argument('--ee-weight', type=float, default=.15,
-                        help="Weighting for the pos difference in the reward")
-    parser.add_argument('--ee-inner-weight', type=float, default=-40,
-                        help="Coefficient for pos difference exponentiation in reward")
-
-    parser.add_argument('--com-weight', type=float, default=.1,
-                        help="Weighting for the com difference in the reward")
-    parser.add_argument('--com-inner-weight', type=float, default=-10,
-                        help="Coefficient for com difference exponentiation in reward")
-
-    parser.add_argument('--p-gain', type=float, default=300,
-                        help="P for the PD controller")
-    parser.add_argument('--d-gain', type=float, default=50,
-                        help="D for the PD controller")
-
     args = parser.parse_args()
-
-    env = DartDeepMimicEnv(args.control_skel_path, args.asf_path,
-                           args.ref_motion_path,
-                           args.state_mode, args.action_mode,
-                           args.p_gain, args.d_gain,
-                           args.pos_init_noise, args.vel_init_noise,
-                           args.reward_cutoff,
-                           args.pos_weight, args.pos_inner_weight,
-                           args.vel_weight, args.vel_inner_weight,
-                           args.ee_weight, args.ee_inner_weight,
-                           args.com_weight, args.com_inner_weight,
-                           args.max_torque,
-                           args.default_damping, args.default_spring,
-                           args.visualize,
-                           args.simsteps_per_dataframe,
-                           args.window_width, args.window_height)
-
+    env = parser.get_env()
     #####################################
     # END COPY-PASTE FROM DARTDEEPMIMIC #
     #####################################
 
-    train(env, args.train_save_interval, args.train_params_prefix,
-          args.train_num_timesteps)
+    train(env,
+          initial_params_path=args.initial_params_path,
+          save_interval=args.train_save_interval,
+          out_prefix=args.output_params_prefix,
+          num_timesteps=args.train_num_timesteps,
+          num_cpus=args.num_cpus)
