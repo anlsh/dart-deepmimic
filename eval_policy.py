942e3776cde6047c18131c1475746c384c6ec0f3
anish <anish.moorthy@gmail.com>
2018-09-10 23:31:03 -0400

    [VISAK-TEST] Make eval_policy run visak environment

--- eval_policy.py
+++ eval_policy.py
@@ -1,5 +1,6 @@
 import argparse
-from ddm_argparse import DartDeepMimicArgParse
+# from ddm_argparse import DartDeepMimicArgParse
+from visak_dartdeepmimic import VisakDartDeepMimicArgParse
 from baselines.common import tf_util as U
 from baselines.ppo1 import mlp_policy, pposgd_simple
 import numpy as np
@@ -24,11 +25,9 @@ class PolicyLoaderAgent(object):
 
 if __name__ == "__main__":
 
-    parser = DartDeepMimicArgParse()
+    parser = VisakDartDeepMimicArgParse()
     parser.add_argument("--params-prefix", required=True, type=str)
     args = parser.parse_args()
-    env = parser.get_env()
-
     env = parser.get_env()
 
     U.make_session(num_cpu=1).__enter__()
@@ -45,18 +44,6 @@ if __name__ == "__main__":
         ob = env.reset(pos_stdv=0, vel_stdv=0)
         done = False
         while not done:
-            action = np.multiply(agent.act(ob, reward, done), 0)
+            action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
             env.render()
-
-
-    # for i in range(episode_count):
-    #     ob = env.reset()
-    #     while True:
-    #         action = agent.act(ob, reward, done)
-    #         ob, reward, done, _ = env.step(action)
-    #         if done:
-    #             break
-    #         # Note there's no env.render() here. But the environment still can open window and
-    #         # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-    #         # Video is not recorded every episode, see capped_cubic_video_schedule for details.
