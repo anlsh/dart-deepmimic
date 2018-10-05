417b3bbca9e589474009c4f7f438a6aef9ba15a2
anish <anish.moorthy@gmail.com>
2018-10-05 11:35:42 -0400

    Do a thing which hsouldnt matter

--- visak_dartdeepmimic.py
+++ visak_dartdeepmimic.py
@@ -230,12 +230,12 @@ def PID(skel, target):
     q = skel.q[6:]
     qdot = skel.dq[6:]
 
-    # ndofs = len(q)
+    ndofs = len(q)
 
-    # tau = np.zeros((ndofs - 6,))
-    tau = np.multiply(kp, (target - q)) - np.multiply(kd, qdot)
-    # for i in range(ndofs - 6):
-    #     tau[i] = -kp[i] * (q[i] - target[i]) - kd[i] * qdot[i]
+    tau = np.zeros((ndofs - 6,))
+    # tau = np.multiply(kp, (target - q)) - np.multiply(kd, qdot)
+    for i in range(ndofs - 6):
+        tau[i] = -kp[i] * (q[i] - target[i]) - kd[i] * qdot[i]
 
     torqs = ClampTorques(tau)
 
