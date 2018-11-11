98c776f6f887d9d96ed981a6d55d02a02956ca8b
anish <anish.moorthy@gmail.com>
2018-11-11 16:40:54 -0500

    Chage train around a bit to make it work

--- train_jesus.py
+++ train_jesus.py
@@ -1,6 +1,9 @@
-from baselines.common.cmd_util import make_dart_env, common_arg_parser
+# from baselines.common.cmd_util import make_dart_env
+from baselines.common.cmd_util import common_arg_parser
 from baselines.common import tf_util as U
+from baselines.bench import Monitor
 from baselines import logger
+import tensorflow as tf
 
 from Humanoid3D_cartesian_jesus import DartHumanoid3D_cartesian_jesus
 from baselines.ppo1 import mlp_policy, pposgd_simple
@@ -10,24 +13,25 @@ def make_dart_env(seed):
     env = Monitor(env, logger.get_dir())
     return env
 
-def policy_fn(name, ob_space, ac_space):
-    # TODO Ensure that multiple-layers implementation is really solid
-    return mlp_policy.MlpPolicy(name=name,
-                                ob_space=ob_space, ac_space=ac_space,
-                                hidden_dimension_list=[128,128])
-
 def train(num_timesteps, seed,
-          train_save_interval, output_prefix, ):
+          save_interval, output_prefix, ):
 
-    U.make_session(num_cpu=1).__enter__()
+    sess = U.make_session(num_cpu=1)
+    sess.__enter__()
     env = make_dart_env(seed)
+
+
+    def policy_fn(name, ob_space, ac_space):
+        # TODO Ensure that multiple-layers implementation is really solid
+        return mlp_policy.MlpPolicy(name=name,
+                                    ob_space=ob_space, ac_space=ac_space,
+                                    hidden_dimension_list=[128,128])
 
     def callback_fn(local_vars, global_vars):
         iters = local_vars["iters_so_far"]
         saver = tf.train.Saver()
         if iters % save_interval == 0:
             saver.save(sess, output_prefix + str(iters))
-
 
     pposgd_simple.learn(env, policy_fn,
             max_timesteps=num_timesteps,
@@ -46,7 +50,7 @@ def main():
     # MY CUSTOM ARGS
 
 
-    parser.add_argument('--train-save-interval', type=int, default=100,
+    parser.add_argument('--save-interval', type=int, default=100,
                         help="Interval between saves and stuff")
     parser.add_argument('--output-prefix', required=True,
                         help="Fire prefix of parameter saves")
@@ -66,7 +70,7 @@ def main():
     logger.configure()
 
     train(num_timesteps=args.num_timesteps, seed=args.seed,
-          train_save_interval=args.train_save_interval,
+          save_interval=args.save_interval,
           output_prefix=args.output_prefix)
 
 if __name__ == '__main__':
